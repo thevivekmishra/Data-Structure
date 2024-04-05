16309ca424855b23af5e7c7e9ca09ef7d6ee0c4e
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-05 18:43:17 +0530

    Extreme Print

--- 3_Array/8_extremePrint.cpp
+++ 3_Array/8_extremePrint.cpp
@@ -1,6 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main() {
-    cout << "world ";
+int main()
+{
+    int arr[8] = {10, 20, 30, 40, 50, 60, 70, 80};
+    int size = 8;
+
+    int start = 0;
+    int end = size - 1;
+
+    while (true)
+    {
+        if (start > end)
+            break;
+
+        if (start == end)
+        {
+            cout << arr[start]; // when element is in odd
+        }
+
+        else
+        {
+            cout << arr[start] << " ";
+            cout << arr[end] << " ";
+        }
+        start++;
+        end--;
+    }
+    return 0;
 }

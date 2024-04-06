4722478d5915bbb06794af8f01b7606d71351589
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-07 00:46:42 +0530

    leetCode P.No.75

--- 0_LEET_CODE/1_sortColors.cpp
+++ 0_LEET_CODE/1_sortColors.cpp
@@ -1,4 +1,6 @@
-//leetcode problem no. 75 Sort colors
+// leetcode problem no. 75 Sort colors
+
+// 1st approach using sort function ---> dont use X
 
 // class Solution {
 // public:
@@ -7,4 +9,49 @@
 //     }
 // };
 
+// 2nd approach------------------------------------------------------------
 
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         //sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size();i++){
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 if(nums[i]>nums[j])
+//                 {
+//                     swap(nums[i],nums[j]);
+//                 }
+//             }
+//         }
+//     }
+// };
+
+// 3rd approach 0ms time complexity ---> best approach-------------------------
+
+// class Solution
+// {
+// public:
+//     void sortColors(vector<int> &nums)
+//     {
+//         int l = 0, m = 0, h = nums.size() - 1;
+//         while (m <= h)
+//         {
+//             if (nums[m] == 0)
+//             {
+//                 swap(nums[l], nums[m]);
+//                 l++;
+//                 m++;
+//             }
+//             else if (nums[m] == 1)
+//             {
+//                 m++;
+//             }
+//             else
+//             {
+//                 swap(nums[m], nums[h]);
+//                 h--;
+//             }
+//         }
+//     }
+// };

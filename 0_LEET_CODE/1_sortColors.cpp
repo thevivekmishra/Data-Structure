afac385cce4a6da1873e0efc91631ff17f347f3c
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-07 00:08:04 +0530

    LeetCode P.No.75

---
+++ 0_LEET_CODE/1_sortColors.cpp
@@ -0,0 +1,10 @@
+//leetcode problem no. 75 Sort colors
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//     }
+// };
+
+

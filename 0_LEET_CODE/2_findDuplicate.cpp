09d9d65433f4427bc8e96ed3e37809e9a417213c
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-07 14:16:35 +0530

    leetCode P.No 287

--- 0_LEET_CODE/2_findDuplicate.cpp
+++ 0_LEET_CODE/2_findDuplicate.cpp
@@ -58,3 +58,16 @@
 //         return ans;
 //     }   
 // };
+
+//4th approach-- less time complexity---------------------------------
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//       while(nums[0] != nums[nums[0]])
+//       {
+//         swap(nums[0],nums[nums[0]]);
+//       }
+//       return nums[0];
+//     }   
+// };

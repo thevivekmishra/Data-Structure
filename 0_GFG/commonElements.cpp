369925894d04c1a3b7e5301819ba93aa21370068
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-08 18:44:55 +0530

    GFG common elements prob.

---
+++ 0_GFG/commonElements.cpp
@@ -0,0 +1,43 @@
+// //GFG common elements problem 
+
+// // 1st approach using set data structure-------------------------
+// {
+//     public:    
+//        vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+//         {
+//            vector<int>ans;
+//            set<int>st;
+//            int i,j,k;
+//            i=j=k=0;
+//            while(i<n1 && j<n2 && k<n3)
+//            {
+//                if(A[i]==B[j] && B[j]==C[k])
+//                {
+//                 //   ans.push_back(A[i]);
+//                 st.insert(A[i]);
+//                    i++,j++,k++;
+//                }
+//                else if(A[i]<B[j])
+//                {
+//                    i++;
+//                }
+//                else if(B[j]<C[k])
+//                {
+//                    j++;
+//                }
+//                else
+//                {
+//                    k++;
+//                }
+//            }
+//            for(auto i:st)
+//            {
+//                ans.push_back(i);
+//            }
+//            return ans;
+//         }
+// };
+
+
+
+// 2nd apprach without additional data structure

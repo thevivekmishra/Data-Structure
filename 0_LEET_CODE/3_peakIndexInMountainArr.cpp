1a3ac32fedc562a11dc270aa0ddce3b8a7c872c0
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-09 19:56:15 +0530

    leetCode P.No 852

---
+++ 0_LEET_CODE/3_peakIndexInMountainArr.cpp
@@ -0,0 +1,23 @@
+//LEETCODE peak index in mountain array P.NO 852
+
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//        int s=0;
+//        int e=arr.size()-1;
+//        int mid=(s+e)/2;
+//        while(s<e)
+//        {
+//            if(arr[mid]<arr[mid+1])
+//            {
+//                s=mid+1;
+//            }
+//            else
+//            {
+//                e=mid;
+//            }
+//            mid=(s+e)/2;
+//        } 
+//        return s;
+//     }
+// };

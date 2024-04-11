4bce95fec8a3e2dad2fb7cacfe113279edf0df26
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-11 20:01:23 +0530

    TopicWiseCode

--- PracticeSet/2_findDuplicate.cpp
+++ PracticeSet/2_findDuplicate.cpp
@@ -2,27 +2,37 @@
 using namespace std;
 
 // function
-int findDuplicate(int arr[], int size)
+// int findDuplicate(int arr[], int size)
+// {
+//     for(int i=0;i<size;i++)
+//     {
+//         for(int j=i+1;j<size;j++)
+//         {
+//             if(arr[i]==arr[j])
+//             {
+//                 return arr[i];
+//                 break;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+//less time complexity
+int findDuplicate(int arr[],int size)
 {
-    for(int i=0;i<size;i++)
+    while(arr[0]!=arr[arr[0]])
     {
-        for(int j=i+1;j<size;j++)
-        {
-            if(arr[i]==arr[j])
-            {
-                cout<<arr[i];
-                break;
-            }
-        }
+        swap(arr[0],arr[arr[0]]);
     }
-    return 0;
+    return arr[0];
 }
+
 int main()
 {
-    int arr[6] = {1, 4, 7, 3, 6, 1};
-    int size = 6;
+    int arr[5] = {3,1,3,4,2};
+    int size = 5;
 
-    cout << "Duplicate no is :";
-    findDuplicate(arr, size);
+    cout << "Duplicate no is :"<<findDuplicate(arr, size);
     return 0;
 }

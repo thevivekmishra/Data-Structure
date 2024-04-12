1ba799ec314a443fe37d927d7d57087cd64a09c3
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-13 00:54:04 +0530

    GFG Book Allocation Prob.

---
+++ 0_GFG/bookAllocationProblem.cpp
@@ -0,0 +1,62 @@
+// GFG book allocation problem
+
+
+// class Solution
+// {
+// public:
+//     bool isPossibleSolution(int A[], int N, int M, int sol)
+//     {
+//         int pageSum = 0;
+//         int count = 1;
+//         for (int i = 0; i < N; i++)
+//         {
+//             if (A[i] > sol)
+//             { // sol is mid
+//                 return false;
+//             }
+//             if (pageSum + A[i] > sol)
+//             {
+//                 count++;
+//                 pageSum = A[i];
+//                 if (count > M)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             else
+//             {
+//                 pageSum += A[i];
+//             }
+//         }
+//         return true;
+//     }
+
+//     // Function to find minimum number of pages.
+//     int findPages(int A[], int N, int M)
+//     {
+//         if (M > N)
+//         {
+//             return -1;
+//         }
+
+//         int start = 0;
+//         int end = accumulate(A, A + N, 0); // STL gives sum
+//         int ans = -1;
+
+//         while (start < end)
+//         {
+//             int mid = start + (end - start) / 2;
+//             if (isPossibleSolution(A, N, M, mid))
+//             {
+//                 ans = mid;
+//                 end = mid - 1;
+//             }
+//             else
+//             {
+//                 start = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+

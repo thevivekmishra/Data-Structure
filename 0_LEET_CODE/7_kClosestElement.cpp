a017a07b7379c15912b42aecbc740626450ffba1
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-12 20:10:29 +0530

    leetcode P.No 658

---
+++ 0_LEET_CODE/7_kClosestElement.cpp
@@ -0,0 +1,29 @@
+// leetcode find k closest element in an array P.no 658
+
+//     class Solution
+// {
+// public:
+//     vector<int> findClosestElements(vector<int> &arr, int k, int x)
+//     {
+//         int low = 0;
+//         int high = arr.size() - 1;
+
+//         while ((high - low) >= k)
+//         {
+//             if (x - arr[low] > arr[high] - x)
+//             {
+//                 low++;
+//             }
+//             else
+//             {
+//                 high--;
+//             }
+//         }
+//         vector<int> ans;
+//         for (int i = low; i <= high; i++)
+//         {
+//             ans.push_back(arr[i]);
+//         }
+//         return ans;
+//     }
+// };

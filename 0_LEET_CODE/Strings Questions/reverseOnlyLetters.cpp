ac7dab951f0b9ab64440f838c51f5189bcfe527c
thevivekmishra <vivekmishra89994@gmail.com>
2024-04-16 22:52:12 +0530

    leetCode

---
+++ 0_LEET_CODE/Strings Questions/reverseOnlyLetters.cpp
@@ -0,0 +1,27 @@
+// leetcode reverse only letteres prob.no 917
+
+//     class Solution
+// {
+// public:
+//     string reverseOnlyLetters(string s)
+//     {
+//         int l = 0, h = s.size() - 1;
+//         while (l < h)
+//         {
+//             if (isalpha(s[l]) && isalpha(s[h]))
+//             {
+//                 swap(s[l], s[h]);
+//                 l++, h--;
+//             }
+//             else if (!isalpha(s[l]))
+//             {
+//                 l++;
+//             }
+//             else
+//             {
+//                 h--;
+//             }
+//         }
+//         return s;
+//     }
+// };

dee17662326e3314e2dc38c6d6c6f49975c502f6
thevivekmishra <vivekmishra89994@gmail.com>
2024-07-27 20:34:53 +0530

    Hackerrank

--- 00_HackerRank/LinkedList_Questions/01_Kth_node_from_end.cpp
+++ 00_HackerRank/LinkedList_Questions/01_Kth_node_from_end.cpp
@@ -1,13 +1,13 @@
-void fun(SinglyLinkedListNode *head,int &positionFromTail,int &ans){
-    if(head == 0) return ;
-    fun(head->next,positionFromTail,ans);
-    if(positionFromTail == 0){
-        ans = head->data;
-    }
-    positionFromTail--;
-}
-int getNode(SinglyLinkedListNode *llist,int positionFromTail){
-    int ans = -1;
-    fun(llist,positionFromTail,ans);
-    return ans;
-}
+// void fun(SinglyLinkedListNode *head,int &positionFromTail,int &ans){
+//     if(head == 0) return ;
+//     fun(head->next,positionFromTail,ans);
+//     if(positionFromTail == 0){
+//         ans = head->data;
+//     }
+//     positionFromTail--;
+// }
+// int getNode(SinglyLinkedListNode *llist,int positionFromTail){
+//     int ans = -1;
+//     fun(llist,positionFromTail,ans);
+//     return ans;
+// }
